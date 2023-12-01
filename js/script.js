4c3ed51b7750c5c31163e354631715a3248abda4
Junior DeLonge <147082536+JuniorDeLonge@users.noreply.github.com>
2023-12-01 11:54:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+﻿// Obtenha os elementos uma vez para evitar buscas repetidas no DOM
 var errorCode = document.getElementById('errorCode');
 var errorMessage = document.getElementById('errorMessage');
 
-errorCode.textContent = 'HAHA!';
-errorMessage.textContent = 'Página não encontrada';
+// Verifique se os elementos existem antes de tentar alterar suas propriedades
+if (errorCode) {
+    errorCode.textContent = 'HAHA!';
+} else {
+    console.error('Elemento errorCode não encontrado');
+}
+
+if (errorMessage) {
+    errorMessage.textContent = 'Página não encontrada';
+} else {
+    console.error('Elemento errorMessage não encontrado');
+}
 
 function goBack() {
-    window.history.back();
+    // Verifique se a URL é válida antes de redirecionar
+    var url = "https://www.instagram.com/juniordelonge";
+    try {
+        new URL(url);
+        window.location.href = url;
+    } catch (e) {
+        console.error('URL inválida: ' + url);
+    }
 }
